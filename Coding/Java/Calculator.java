54b410ab5c48c0759507e461e4609f0d9bb3535a
anguyen36clayton <anguyen36@student.clayton.edu>
2024-03-11 03:10:17 -0400

    Added loops for num1 and num2

--- Coding/Java/Calculator.java
+++ Coding/Java/Calculator.java
@@ -4,38 +4,54 @@ import java.util.Scanner;
 public class Calculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+
         boolean continueCalculating = true;
         while (continueCalculating) {
-            try {
-                // Prompt the user to enter the first number
-                System.out.print("Enter the first number: ");
-                int num1 = scanner.nextInt();
-                
-                // Prompt the user to enter the second number
-                System.out.print("Enter the second number: ");
-                int num2 = scanner.nextInt();
-                
-                int addition = num1 + num2;
-                int subtraction = num1 - num2;
-                int multiplication = num1 * num2;
-                
-                // Check if num2 is not zero to avoid division by zero
-                int division = num2 != 0 ? num1 / num2 : 0;
-                
-                // Check if num2 is not zero to avoid modulus by zero
-                int modulus = num2 != 0 ? num1 % num2 : 0;
-
-                System.out.println("Addition: " + num1 + " + " + num2 + " = " + addition);
-                System.out.println("Subtraction: " + num1 + " - " + num2 + " = " + subtraction);
-                System.out.println("Multiplication: " + num1 + " * " + num2 + " = " + multiplication);
-                System.out.println("Division: " + num1 + " / " + num2 + " = " + division);
-                System.out.println("Modulus: " + num1 + " % " + num2 + " = " + modulus);
-            } catch (InputMismatchException e) {
-                System.out.println("Please enter only numbers.");
-                // Consume the invalid input
-                scanner.next();
+            int num1 = 0;
+            boolean validInput = false;
+            while (!validInput) {
+                try {
+                    // Prompt the user to enter the first number
+                    System.out.print("Enter the first number: ");
+                    num1 = scanner.nextInt();
+                    validInput = true;
+                } catch (InputMismatchException e) {
+                    System.out.println("Please enter only numbers.");
+                    // Consume the invalid input
+                    scanner.next();
+                }
             }
+
+            boolean validInputForNum2 = false;
+            int num2 = 0;
+            while (!validInputForNum2) {
+                try {
+                    // Prompt the user to enter the second number
+                    System.out.print("Enter the second number: ");
+                    num2 = scanner.nextInt();
+                    validInputForNum2 = true;
+                } catch (InputMismatchException e) {
+                    System.out.println("Please enter only numbers.");
+                    // Consume the invalid input
+                    scanner.next();
+                }
+            }
+
+            int addition = num1 + num2;
+            int subtraction = num1 - num2;
+            int multiplication = num1 * num2;
+
+            // Check if num2 is not zero to avoid division by zero
+            int division = num2 != 0 ? num1 / num2 : 0;
+
+            // Check if num2 is not zero to avoid modulus by zero
+            int modulus = num2 != 0 ? num1 % num2 : 0;
+
+            System.out.println("Addition: " + num1 + " + " + num2 + " = " + addition);
+            System.out.println("Subtraction: " + num1 + " - " + num2 + " = " + subtraction);
+            System.out.println("Multiplication: " + num1 + " * " + num2 + " = " + multiplication);
+            System.out.println("Division: " + num1 + " / " + num2 + " = " + division);
+            System.out.println("Modulus: " + num1 + " % " + num2 + " = " + modulus);
 
             // Prompt the user to continue or exit
             System.out.print("Do you want to perform another calculation? (yes/no): ");
