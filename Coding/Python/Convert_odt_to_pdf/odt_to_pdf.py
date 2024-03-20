59c9d677d1d2e5b834ba40deca5b3994afc90e98
anguyen36clayton <anguyen36@student.clayton.edu>
2024-03-19 22:58:03 -0400

    Moves ODT files to ODT folder, current file contains PDF files

--- Coding/Python/Convert_odt_to_pdf/odt_to_pdf.py
+++ Coding/Python/Convert_odt_to_pdf/odt_to_pdf.py
@@ -14,19 +14,19 @@ def convert_odt_to_pdf(input_file, output_folder):
         return False, ""
 
 def main():
-    # Create a directory named PDF if it doesn't exist
-    pdf_folder = "PDF"
-    if not os.path.exists(pdf_folder):
-        os.makedirs(pdf_folder)
+    # Create a directory named ODT if it doesn't exist
+    odt_folder = "ODT"
+    if not os.path.exists(odt_folder):
+        os.makedirs(odt_folder)
 
     # Get a list of all .odt files in the current directory
     odt_files = [file for file in os.listdir() if file.endswith(".odt")]
 
-    # Convert each .odt file to PDF and move the PDF file to the PDF folder
+    # Convert each .odt file to PDF and move the ODT file to the ODT folder
     for odt_file in odt_files:
-        if convert_odt_to_pdf(odt_file, pdf_folder):
-            # Move the original .odt file to the PDF folder
-            shutil.move(odt_file, os.path.join(pdf_folder, odt_file))
+        if convert_odt_to_pdf(odt_file, odt_folder):
+            # Move the original .odt file to the ODT folder
+            shutil.move(odt_file, os.path.join(odt_folder, odt_file))
 
 if __name__ == "__main__":
     main()
