e8499c8767c2211251bf33c169d9ee33e15ae8ce
anguyen36clayton <anguyen36@student.clayton.edu>
2024-03-21 01:20:02 -0400

    Fixed bugs

--- Coding/Python/Live Markdown Editor/app.py
+++ Coding/Python/Live Markdown Editor/app.py
@@ -5,11 +5,17 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    markdown_content = ""
+    html_content = ""
+
     if request.method == 'POST':
+        # Get Markdown content from form submission
         markdown_content = request.form['markdown_content']
+
+        # Convert Markdown to HTML
         html_content = markdown2.markdown(markdown_content)
-        return render_template('index.html', html_content=html_content, markdown_content=markdown_content)
-    return render_template('index.html')
+
+    return render_template('index2.html', markdown_content=markdown_content, html_content=html_content)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
