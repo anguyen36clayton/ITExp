110e5482abc9b934c6029a45fbf0e281fbacffa9
anguyen36clayton <anguyen36@student.clayton.edu>
2024-04-10 19:07:25 -0400

    Fixed bugs and made socketIO local

--- Coding/Python/Chat Server/app.py
+++ Coding/Python/Chat Server/app.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template, request, redirect, url_for
-from flask_socketio import SocketIO
+# app.py
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
 # This will store messages temporarily. In a real-world application, you'd use a database.
@@ -15,8 +18,9 @@ def index():
 def handle_message(data):
     username = data['username']
     message = data['message']
-    messages.append({'username': username, 'message': message})
-    socketio.emit('new_message', {'username': username, 'message': message}, broadcast=True)
+    timestamp = datetime.now().strftime('%m/%d/%y %I:%M %p')  # Adjusted timestamp format
+    messages.append({'username': username, 'message': message, 'timestamp': timestamp})
+    emit('new_message', {'username': username, 'message': message, 'timestamp': timestamp}, broadcast=True)
 
 if __name__ == '__main__':
     socketio.run(app, debug=True, host='0.0.0.0', port=5000)
