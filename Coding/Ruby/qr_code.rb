bc48202f5b34bc97479779204c779e7c688a2320
anguyen36clayton <anguyen36@student.clayton.edu>
2024-04-12 21:10:32 -0400

    Added generation of image

--- Coding/Ruby/qr_code.rb
+++ Coding/Ruby/qr_code.rb
@@ -1,22 +1,18 @@
 require 'rqrcode'
-require 'rqrcode_png'
 
-def generate_qr_code(url, filename)
+def display_qr_code(url)
   qr = RQRCode::QRCode.new(url)
-  png = qr.as_png(
-    resize_gte_to: false,
-    resize_exactly_to: false,
-    fill: 'white',
-    color: 'black',
-    size: 120
-  )
-  IO.binwrite(filename, png.to_s)
-  puts "QR code generated successfully as '#{filename}'."
+  qr.modules.each do |row|
+    row.each do |cell|
+      print cell ? '██' : '  '
+    end
+    puts
+  end
 end
 
 puts "Please enter the URL:"
 url = gets.chomp
 
-puts "Generating QR code..."
-generate_qr_code(url, 'qr_code.png')
+puts "Generating and displaying QR code..."
+display_qr_code(url)
 
