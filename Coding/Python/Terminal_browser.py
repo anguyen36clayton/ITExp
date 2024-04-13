60336c4dc46029a62df40cc1e2eb9d3fb03ea223
anguyen36clayton <anguyen36@student.clayton.edu>
2024-04-12 22:21:13 -0400

    Made browser fancier

--- Coding/Python/Terminal_browser.py
+++ Coding/Python/Terminal_browser.py
@@ -1,9 +1,19 @@
 import webbrowser
+from colorama import init, Fore, Style
+
+# Initialize colorama to support colored text on Windows
+init()
 
 def open_website(url):
     webbrowser.open(url)
 
-if __name__ == "__main__":
-    website_url = input("Enter the URL of the website you want to open: ")
-    open_website(website_url)
+def fancy_print(text, color=Fore.WHITE):
+    print(color + text + Style.RESET_ALL)
 
+if __name__ == "__main__":
+    fancy_print("Welcome to Fancy Website Opener!", Fore.CYAN)
+    fancy_print("-" * 40, Fore.YELLOW)
+    website_url = input(Fore.GREEN + "Enter the URL of the website you want to open: ")
+    fancy_print("-" * 40, Fore.YELLOW)
+    fancy_print("Opening website...", Fore.MAGENTA)
+    open_website(website_url)
