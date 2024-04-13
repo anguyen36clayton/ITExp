867c2d34ade44b0815bbba3c83db280efc434d4a
anguyen36clayton <anguyen36@student.clayton.edu>
2024-04-12 21:11:14 -0400

    Fixed generation of QR code on terminal

--- Coding/Ruby/qr_code.rb
+++ Coding/Ruby/qr_code.rb
@@ -1,4 +1,5 @@
 require 'rqrcode'
+require 'chunky_png'
 
 def display_qr_code(url)
   qr = RQRCode::QRCode.new(url)
@@ -10,9 +11,27 @@ def display_qr_code(url)
   end
 end
 
+def generate_qr_code_png(url, file_name)
+  qr = RQRCode::QRCode.new(url)
+  png = qr.as_png(
+    resize_gte_to: false,
+    resize_exactly_to: false,
+    fill: 'white',
+    color: 'black',
+    size: 120,
+    border_modules: 4
+  )
+
+  png.save(file_name)
+  puts "QR code saved as #{file_name}"
+end
+
 puts "Please enter the URL:"
 url = gets.chomp
 
-puts "Generating and displaying QR code..."
+puts "Generating and displaying QR code in terminal..."
 display_qr_code(url)
+
+puts "Generating QR code PNG file..."
+generate_qr_code_png(url, "qr_code.png")
 
